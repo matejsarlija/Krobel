501a149c3776538d99da05a1757049a12b85ce9d
matejsa <matejsarlija1@gmail.com>
2022-03-01 15:40:34 +0100

    modeling...

--- Models/TodoItem.cs
+++ Models/TodoItem.cs
@@ -1,9 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Krobel.Models;
 
 public class TodoItem
 {
     public long Id { get; set; }
     public string? Name { get; set; }
-    public bool IsComplete { get; set; }
-    public string? Secret { get; set; }
+    public DateTime? DoB { get; set; }
+    
+    [DataType(DataType.PhoneNumber)]
+    public string? PhoneNumber { get; set; }
+    
+    public Gender EmployeeGender { get; set; }
+}
+
+public enum Gender
+{
+    Male,
+    Female
 }
