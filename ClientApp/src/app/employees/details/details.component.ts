94bf8c02791cbcc0686ccb27c991fe487ca13930
matejsa <matejsarlija1@gmail.com>
2022-03-04 09:56:52 +0100

    Details page

--- ClientApp/src/app/employees/details/details.component.ts
+++ ClientApp/src/app/employees/details/details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Employee } from '../employee';
+import { EmployeesService } from '../employees.service';
 
 @Component({
   selector: 'app-details',
@@ -7,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsComponent implements OnInit {
 
-  constructor() { }
+  id!: number;
+  employee!: Employee;
+
+  constructor(
+    public employeesService: EmployeesService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.id = this.route.snapshot.params['employeeId'];
+    this.employeesService.getEmployee(this.id).subscribe((data: Employee) =>
+    {
+      this.employee = data;
+    })
   }
 
 }
